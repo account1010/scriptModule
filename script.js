01631d68c701146027910474e6b8ea57a9bcc57d
important-api <156857844+important-api@users.noreply.github.com>
2024-01-24 16:17:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//time command
+
 const time = {
     out:function(callbackfn,durationSecond){
         if(typeof callbackfn !== "function"){
@@ -17,4 +19,35 @@ const time = {
             return "waiting..."
         }
     }
+}
+
+//receiveData command
+async function receiveData(url) {
+    if (typeof url === "string") {
+        try{
+            const data = await fetch(url);
+            const response = await data.text()
+            console.log(response)
+        }
+        catch(error){
+            throw new TypeError("failed to receiveData")
+        }
+    }
+}
+
+//write command
+function write(...data) {
+    const result = data.map(item => {
+        if (typeof item === 'symbol') {
+            return `Symbol(${item.description || 'Symbol'})`;
+        } else if (item === null) {
+            return 'null';
+        } else if (typeof item === 'function') {
+            return 'function';
+        } else {
+            return item;
+        }
+    });
+    console.log(result);
+    console.log(result.join(" "));
 }
